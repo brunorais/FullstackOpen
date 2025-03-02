c9d41fd63b31930073a8acfd86fd572e35feb2e7
brunorais <116595783+brunorais@users.noreply.github.com>
2025-03-02 16:34:06 +0100

    new file:   part1/unicafe_paso_3/.gitignore 	new file:   part1/unicafe_paso_3/README.md 	new file:   part1/unicafe_paso_3/index.html 	new file:   part1/unicafe_paso_3/package-lock.json 	new file:   part1/unicafe_paso_3/package.json 	new file:   part1/unicafe_paso_3/public/vite.svg 	new file:   part1/unicafe_paso_3/src/app.jsx 	new file:   part1/unicafe_paso_3/src/main.jsx 	new file:   part1/unicafe_paso_3/vite.config.js

---
+++ part1/unicafe_paso_3/src/app.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+
+const Header = ({text}) => {
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const SubTitle  = ({text}) => {
+  return(
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return(
+    <div>
+       <div>good {props.list_options[0]}</div>
+       <div>neutral {props.list_options[1]}</div>
+       <div>bad {props.list_options[2]}</div>
+       <div>all {props.list_options[0] + props.list_options[1] + props.list_options[2]}</div>
+       <div>
+        average {(props.list_options[0] - props.list_options[2])/(props.list_options[0] + props.list_options[1] + props.list_options[2])}
+      </div>
+      <div>
+        positive {(props.list_options[0]*100)/(props.list_options[0] + props.list_options[1] + props.list_options[2])} %
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const header_text = 'give feedback'
+  const subTitle_text = 'statistics'
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    setBad(updateBad)
+  }
+
+  const list_options = [good, neutral, bad]
+
+  return (
+    <div>
+      <Header text = {header_text}></Header>
+      <Button handleClick = {handleGoodClick} text='good' />
+      <Button handleClick = {handleNeutralClick} text='neutral' />
+      <Button handleClick = {handleBadClick} text='bad' />
+      <SubTitle text = {subTitle_text}></SubTitle>
+      <Statistics list_options = {list_options}></Statistics>
+    </div>
+  )
+}
+
+export default App
